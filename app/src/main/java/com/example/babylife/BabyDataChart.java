ef339b532b76ca1856087f35d54188e4c187ff54
Graham Murray <GLMurray@student.fullsail.edu>
2024-01-22 16:01:48 -0500

    1. Added ability to parse and query data from the previously created sqlite databases 2. Then displays in database(only feeding, amount data) 3.Sets child name so you can look at data for specific child and seperate that between each child. 4.Then displays in bar graph. 5.Added spinners so you can change between datasets and databases 6.Updated gradle files.
    TODO: Add Sleep and diaper data, add duration for feeding data

--- app/src/main/java/com/example/babylife/BabyDataChart.java
+++ app/src/main/java/com/example/babylife/BabyDataChart.java
@@ -2,84 +2,175 @@ package com.example.babylife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.example.babylife.contracts.FeedingLogContract;
+import com.example.babylife.helpers.SQLiteAddAFeedingHelper;
+import com.example.babylife.sqlitefiles.SQLiteBabyName;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BabyDataChart extends AppCompatActivity {
+    private SQLiteAddAFeedingHelper feedingHelper;
+    private SQLiteBabyName dbNameHelper;
+    Spinner spinnerChildName, spinnerType, spinnerTypeFocused;
 
+    BarChart barChart;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baby_data_chart);
+        spinnerChildName=findViewById(R.id.spinnerChartChildName);
+        spinnerType=findViewById(R.id.spinnerChartType);
+        spinnerTypeFocused =findViewById(R.id.spinnerChartTypeFocused);
 
-        BarChart barChart = findViewById(R.id.chart);
-        BarData data = new BarData(getDataSet());
+        dbNameHelper= new SQLiteBabyName(this);
+        feedingHelper = new SQLiteAddAFeedingHelper(this);
+
+        //Load child names
+        loadBabyNames();
+
+        //Setup BarChart.
+        barChart = findViewById(R.id.chart);
+
+
+        //Set bar data dependent on child name selected
+        ArrayList<Float> feedingData = getFeedingDataFromDB(spinnerChildName.getSelectedItem().toString());
+        BarData data = new BarData(getDataSet(feedingData));
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(getXAxisValues(feedingData.size())));
         barChart.setData(data);
-
         barChart.animateXY(2000, 2000);
         barChart.invalidate();
+
+
+
     }
 
-    private ArrayList<IBarDataSet> getDataSet() {
+    //Will change chart data when different child name is selected
+    private void updateChartData() {
+        String selectedChild = spinnerChildName.getSelectedItem().toString();
+        ArrayList<Float> feedingData = getFeedingDataFromDB(selectedChild);
+        BarData data = new BarData(getDataSet(feedingData));
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(getXAxisValues(feedingData.size())));
+        barChart.setData(data);
+        barChart.notifyDataSetChanged(); // Notify the chart of data changes
+        barChart.invalidate(); // Refresh the chart
+    }
+
+    //Loads baby names from SQLITEBABYNAMES and sets up the listener for the first spinner.
+    private void loadBabyNames() {
+        List<String> childNames = dbNameHelper.getAllChildNames();// Fetch names from the database
+
+        if (!childNames.isEmpty()) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, childNames);
+            spinnerChildName.setAdapter(adapter);
+
+            spinnerChildName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    updateChartData();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+        } else {
+        }
+        Log.d("Add Child Name", "Child list size: " + childNames.size());
+    }
+
+
+    //Sets up the bargraph data dependent on the getFeed
+    //TODO: Add 2 other types (SLEEPDATA, DIAPERDATA) and make that return correct fields.
+    private ArrayList<IBarDataSet> getDataSet(ArrayList<Float> feedingData) {
         ArrayList<IBarDataSet> dataSets = new ArrayList<>();
 
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
 
-        ArrayList valueSet1 = new ArrayList();
-        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
-        valueSet1.add(v1e1);
-        BarEntry v1e2 = new BarEntry(120.000f, 1); // Feb
-        valueSet1.add(v1e2);
-        BarEntry v1e3 = new BarEntry(70.000f, 2); // Mar
-        valueSet1.add(v1e3);
-        BarEntry v1e4 = new BarEntry(90.000f, 3); // Apr
-        valueSet1.add(v1e4);
-        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
-        valueSet1.add(v1e5);
-        BarEntry v1e6 = new BarEntry(105.000f, 5); // Jun
-        valueSet1.add(v1e6);
+        //Loop for amount in feeding data
+        for (int i = 0; i < feedingData.size(); i++) {
+            // Use the feedingData that's passed as a parameter
+            Log.d("ChartData", "Amount: "+ feedingData.get(i));
+            valueSet1.add(new BarEntry(feedingData.get(i), feedingData.get(i)));
+        }
 
-        ArrayList valueSet2 = new ArrayList();
-        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
-        valueSet2.add(v2e1);
-        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
-        valueSet2.add(v2e2);
-        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
-        valueSet2.add(v2e3);
-        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
-        valueSet2.add(v2e4);
-        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
-        valueSet2.add(v2e5);
-        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
-        valueSet2.add(v2e6);
-
-        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Aoma Murray");
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, spinnerChildName.getSelectedItem().toString()); // Replace with actual child's name
         barDataSet1.setColor(Color.rgb(0, 155, 0));
-        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Samuel Murray");
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
-
-        dataSets = new ArrayList();
         dataSets.add(barDataSet1);
-        dataSets.add(barDataSet2);
         return dataSets;
     }
 
-    private ArrayList<String> getXAxisValues() {
+    //Returns an arraylist of the data for the amount that is in the feeding data database
+    //QUERIES database for "amount," and returns that in descending order.
+    private ArrayList<Float> getFeedingDataFromDB(String childName) {
+        //TODO FIX
+        ArrayList<Float> feedingData = new ArrayList<>();
+        SQLiteDatabase db = feedingHelper.getReadableDatabase();
+
+        String selection = FeedingLogContract.FeedingLogEntry.COLUMN_NAME + " = ?";
+        String[] selectionArgs = { childName };
+        String sortOrder = FeedingLogContract.FeedingLogEntry.COLUMN_DATE + " DESC";
+
+        Cursor cursor = db.query(
+                FeedingLogContract.FeedingLogEntry.TABLE_NAME,
+                new String[] {FeedingLogContract.FeedingLogEntry.COLUMN_AMOUNT},
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+        try {
+            // Iterate over the cursor to get amounts
+            while (cursor.moveToNext()) {
+                String amountStr = cursor.getString(cursor.getColumnIndexOrThrow(FeedingLogContract.FeedingLogEntry.COLUMN_AMOUNT));
+                try {
+                    // Convert the amount string to float and add to the list
+                    float amount = Float.parseFloat(amountStr);
+                    feedingData.add(amount);
+                    Log.d("FeedingData", "Amount parsed: " + amount); // Add this line for debugging
+
+                } catch (NumberFormatException e) {
+                    // Handle parse error
+                    Log.e("getFeedingDataFromDB", "Error parsing amount to float: " + amountStr, e);
+                }
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        db.close();
+        Log.e("BABYDATA", feedingData.toString());
+        return feedingData;
+    }
+
+
+    //Sets the xAxis values based on each entry that is in current database.
+    private ArrayList<String> getXAxisValues(int numberOfEntries) {
         ArrayList<String> xAxis = new ArrayList();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
+       for(int i = 0; i <numberOfEntries +1; i++){
+           xAxis.add("Entry "+ (numberOfEntries -i));
+       }
         return xAxis;
     }
 }
